d64684ac5ce111b5c9264f010f9205ee4eb51162
Matthew Holey <matthewbholey@gmail.com>
2017-11-27 06:59:55 -0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-console.log("User flipped " + cardThree);
-console.log("User flipped " + cardFour);
+var cards = ["queen", "queen", "king", "king"];
+var cardOne = cards [0];
+var cardTwo = cards [2];
+var cardsInPlay = [];
+	cardsInPlay.push("cardOne");
+	console.log("User flipped " + cardOne);
+	cardsInPlay.push("cardTwo");
+	console.log("User flipped " + cardTwo);
+if (cardsInPlay.length === 2) {
+	console.log("2 Cards In Play");
+} else if (cardsInPlay[0] === cardsInPlay[2]) {
+	alert("You found a match!");
+} else if (cardsInPlay[0] !== cardsInPlay[2]) {
+	alert("Sorry, try again.");
+}
+//console.log("User flipped " + cardThree);
+//console.log("User flipped " + cardFour);
+
+/*var cardThree = "king";
+var cardFour = "king";*/
